40f6f8b9337f394cb9f41b367a5e5d4ce098b8d4
ErickElizondo <erickelizondoalt20@gmail.com>
2021-10-01 19:32:06 -0600

    Changed document limit size

--- server.js
+++ server.js
@@ -7,12 +7,11 @@ var cors = require('cors')
 connectDB();
 const app = express();
 
-app.use(express.json());
+app.use(express.json({limit: '16mb'}));
 app.use(cors());
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/private', require('./routes/private'));
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
