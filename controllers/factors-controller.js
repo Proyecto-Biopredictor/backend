8311d3b3fecf426ce774df80b4a4fb36066fdd3a
maurigamg <44620109+maurigamg@users.noreply.github.com>
2021-10-17 23:56:28 -0600

    BIOP-59 BIOP-64 validación hecha

--- controllers/factors-controller.js
+++ controllers/factors-controller.js
@@ -1,9 +1,12 @@
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 const Factor = require('../models/Factor');
+const Record = require('../models/Record');
 
 const HttpError = require('../models/http-error');
 const Bioprocess = require('../models/Bioprocess');
+
+const ObjectId = require('mongodb').ObjectId;
 
 //Get a Factor by ID
 const getFactorById = async (req, res, next) => {
@@ -45,22 +48,37 @@ const createFactor = async (req, res, next) => {
     type
   });
 
-   let bioprocess;
-   try {
-     bioprocess = await Bioprocess.findById(req.body.bioprocessID, {image: 0});
-    
-   } catch (err) {
-     const error = new HttpError(
-       'Could not fetch bioprocess, please try again.',
-       500
-     );
-     return next(error);
-   }
+  let bioprocess;
+  try {
+    bioprocess = await Bioprocess.findById(req.body.bioprocessID, { image: 0 });
 
-   if (!bioprocess) {
-     const error = new HttpError('Could not find bioprocess for provided id.', 404);
-     return next(error);
-   }
+  } catch (err) {
+    const error = new HttpError(
+      'Could not fetch bioprocess, please try again.',
+      500
+    );
+    return next(error);
+  }
+
+  if (!bioprocess) {
+    const error = new HttpError('Could not find bioprocess for provided id.', 404);
+    return next(error);
+  }
+
+  let records;
+  try {
+    records = await Record.find({ "bioprocessID": new ObjectId(bioprocess._id) });
+  } catch (err) {
+    const error = new HttpError(
+      'Fetching records failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  for (let index = 0; index < records.length; index++) {
+    records[index].values[0][createdFactor.name] = "";
+  }
 
   try {
     const sess = await mongoose.startSession();
@@ -68,6 +86,10 @@ const createFactor = async (req, res, next) => {
     await createdFactor.save({ session: sess });
     bioprocess.factors.push(createdFactor._id);
     await bioprocess.save({ session: sess });
+    for (let index = 0; index < records.length; index++) {
+      records[index].values[0] = Object.assign({}, records[index].values[0]);
+      await records[index].save({ session: sess });
+    }
     await sess.commitTransaction();
 
   } catch (err) {
@@ -83,7 +105,7 @@ const createFactor = async (req, res, next) => {
 };
 
 const getFactors = async (req, res, next) => {
-  
+
   let factors;
   try {
     factors = await Factor.find();
@@ -120,7 +142,7 @@ const deleteFactor = async (req, res, next) => {
   }
 
   try {
-    bioprocess = await Bioprocess.findById(bioprocessId, {image: 0});
+    bioprocess = await Bioprocess.findById(bioprocessId, { image: 0 });
   } catch (err) {
     const error = new HttpError(
       'Something went wrong, could not find bioprocess.',
@@ -129,13 +151,32 @@ const deleteFactor = async (req, res, next) => {
     return next(error);
   }
 
+  let records;
+  try {
+    records = await Record.find({ "bioprocessID": new ObjectId(bioprocess._id) });
+  } catch (err) {
+    const error = new HttpError(
+      'Fetching records failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  for (let index = 0; index < records.length; index++) {
+    delete records[index].values[0][factor.name];
+  }
+
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
     const factorIndex = bioprocess.factors.indexOf(factorId);
-    bioprocess.factors.splice(factorIndex,1);
+    bioprocess.factors.splice(factorIndex, 1);
     await factor.remove({ session: sess });
     await bioprocess.save();
+    for (let index = 0; index < records.length; index++) {
+      records[index].values[0] = Object.assign({}, records[index].values[0]);
+      await records[index].save({ session: sess });
+    }
     await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError(
@@ -150,7 +191,7 @@ const deleteFactor = async (req, res, next) => {
 
 const updateFactor = async (req, res, next) => {
 
-  const { name, description, isDependent, bioprocessID, type} = req.body;
+  const { name, description, isDependent, bioprocessID, type } = req.body;
   const factorId = req.params.fid;
 
   let factor;
@@ -163,15 +204,49 @@ const updateFactor = async (req, res, next) => {
     );
     return next(error);
   }
-
+  let oldName = factor.name;
   factor.name = name;
   factor.description = description;
   factor.isDependent = isDependent;
   factor.bioprocessID = bioprocessID;
   factor.type = type;
 
+  let records;
   try {
+    records = await Record.find({ "bioprocessID": new ObjectId(bioprocessID) });
+  } catch (err) {
+    const error = new HttpError(
+      'Fetching records failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  let recordsdHasChange = false;
+  if (records.length > 0) {
+    if (!records[0].values[0].hasOwnProperty(factor.name)) {
+
+      for (let index = 0; index < records.length; index++) {
+        records[index].values[0][factor.name] = records[index].values[0][oldName];
+        delete records[index].values[0][oldName];
+      }
+
+      recordsdHasChange = true;
+    }
+  }
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
     await factor.save();
+    if (recordsdHasChange) {
+      for (let index = 0; index < records.length; index++) {
+        records[index].values[0] = Object.assign({}, records[index].values[0]);
+        await records[index].save({ session: sess });
+      }
+    }
+    await sess.commitTransaction();
+
   } catch (err) {
     const error = new HttpError(
       'Something went wrong, could not update factor.',
@@ -195,9 +270,9 @@ const getFactorsFromBio = async (req, res, next) => {
     );
     return next(error);
   }
-  let bioprocess =[];
+  let bioprocess = [];
   try {
-    bioprocess = await Bioprocess.findById(bioprocessId, {image: 0});
+    bioprocess = await Bioprocess.findById(bioprocessId, { image: 0 });
   } catch (err) {
     const error = new HttpError(
       'Something went wrong, could not fetch bioprocess.',
@@ -209,10 +284,10 @@ const getFactorsFromBio = async (req, res, next) => {
   let factorsFromBio = [];
 
   factors.forEach(arrayitem => {
-    if(arrayitem.bioprocessID == bioprocessId){
+    if (arrayitem.bioprocessID == bioprocessId) {
       factorsFromBio.push(arrayitem);
     }
-    
+
   });
 
 
